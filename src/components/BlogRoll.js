64617ef2dccb3662f172d2738aa85f72ba6b0618
Nabil Boag <nabilgod@gmail.com>
2019-08-06 12:12:59 +0000

    Changing spelling of alt tag

--- src/components/BlogRoll.js
+++ src/components/BlogRoll.js
@@ -24,7 +24,7 @@ class BlogRoll extends React.Component {
                       <PreviewCompatibleImage
                         imageInfo={{
                           image: post.frontmatter.featuredimage,
-                          alt: `featured image thumbnail for post ${
+                          alt: `featured images thumbnail for post ${
                             post.title
                           }`,
                         }}
